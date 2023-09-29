010b9d35ef93ee65eb4c91d45c12658b107727f4
suicideDuck22 <rdorneles64@gmail.com>
2023-09-29 09:12:23 -0300

    Setup tests and health_check endpoint.

---
+++ tests/health_check.rs
@@ -0,0 +1,38 @@
+use std::net::TcpListener;
+
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+    let url = format!("http://{}/health_check", address);
+
+    // Act
+    let response = client
+        .get(&url)
+        .send()
+        .await
+        .expect("Failed to execute the request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("127.0.0.1:{}", port)
+}
