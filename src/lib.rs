de0824e9c0c4bfc1c76fcf8818dc7df2f342dbbf
suicideDuck22 <rdorneles64@gmail.com>
2023-10-02 13:29:29 -0300

    Created subscription initial tests and Subscribe endpoint boilerplate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
-use std::net::TcpListener;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct NewSubFormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(data: web::Form<NewSubFormData>) -> HttpResponse {
+    println!("Name: {}", data.name);
+    println!("Email: {}", data.email);
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,15 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -12,7 +22,7 @@ async fn health_check_works() {
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
-    let url = format!("http://{}/health_check", address);
+    let url = format!("{}/health_check", address);
 
     // Act
     let response = client
@@ -26,13 +36,56 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port.");
-    let port = listener.local_addr().unwrap().port();
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let url = format!("{}/subscriptions", &address);
 
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
+    // Act
+    let body = "name=le%20renas&email=rdorneles%40@gmail.com";
+    let response = client
+        .post(&url)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute the request.");
 
-    format!("127.0.0.1:{}", port)
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let url = format!("{}/subscriptions", &address);
+
+    let test_cases = vec![
+        ("name=le%20renas", "missing the email"),
+        ("email=rdorneles%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&url)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute the request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
